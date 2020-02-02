1abef8e7dedad595498d7c67895ea6e79be12b46
홍성준 <sjhong@jjsoft.kr>
2020-02-02 18:53:44 +0900

    [new]: 카드 리스트 제작 [UX/UI]: 새로운 UI 제작 [UX/UI]: 바탕 제작

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="app">
+    <div class="container">
+      <div class="input-area">
+        input
+      </div>
+      <div class="content">
+        <div class="card" v-for="item in [1, 2, 3, 4, 5, 6]" :key="item">
+          <h1>{{ item }}</h1>
+          <p>card</p>
+          <p>card</p>
+          <p>card</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class App extends Vue {}
+export default {};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.app {
+  height: 100%;
+  background-color: #71b9c1;
+  .container {
+    height: 100%;
+    background-color: #f6f5f1;
+    --padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    .input-area {
+      background-color: #f6f5f1;
+      box-shadow: inset 5px 5px 10px #d1d0cd, inset -5px -5px 10px #ffffff;
+      height: 50px;
+      border-radius: 10px;
+      margin-bottom: 15px;
+      padding: 10px;
+      margin: 15px;
+    }
+    .content {
+      flex: 1;
+      border-radius: 10px;
+      background-color: #f6f5f1;
+      overflow: auto;
+      padding: 0 15px;
+      .card {
+        border-radius: 20px;
+        background: #f6f5f1;
+        box-shadow: 5px 5px 10px #d1d0cd, -5px -5px 10px #ffffff;
+        padding: 10px;
+        margin-bottom: 30px;
+      }
+    }
+  }
 }
 </style>
